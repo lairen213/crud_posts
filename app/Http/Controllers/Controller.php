c42cef03139caf972535537272b8ffac11e680e9
Sergei <doctorradial@gmail.com>
2022-06-10 19:26:30 +0300

    Changed Datetime to Corse

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
 use DateTime;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
@@ -14,7 +15,6 @@ class Controller extends BaseController
 
     //Method for beautify datetime
     public function beautifyDateTime($datetime){
-        $startTime = new Datetime($datetime);
-        return $startTime->format('F').' '.(int)$startTime->format('d').', '.$startTime->format('Y').' at '.$startTime->format('H') . ':' . $startTime->format('m');
+        return Carbon::parse($datetime)->format('F d, Y').' at '.Carbon::parse($datetime)->format('H:m');
     }
 }
